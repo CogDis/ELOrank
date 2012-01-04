904d360183cec9fcb1e0b82a6355a6a6939b6ed4
Elias Woods <EliasWoods@gmail.com>
2012-01-04 11:28:19 -0500

    Add custom k factor for the environment

--- src/main/java/com/precipicegames/elorank/EnvironmentRankEntity.java
+++ src/main/java/com/precipicegames/elorank/EnvironmentRankEntity.java
@@ -1,11 +1,22 @@
 package com.precipicegames.elorank;
 
 public class EnvironmentRankEntity extends RankEntity {
-
-	@Override
+	public static double kFactor = 7;
 	protected void save() {
-		// TODO Auto-generated method stub
+		// Do nothing!
 
 	}
+	protected double calculate(double comparedrate, double result) {
+		double mer = getRank();
+		double delta = mer - comparedrate;
+		double chance = 1.0/(1.0 +Math.pow(10, delta/400.0));
+		double ratedelta = RankEntity.kFactor*(result - chance);
+		if(ratedelta > kFactor)
+			ratedelta = kFactor;
+		if(ratedelta < -kFactor)
+			ratedelta = -kFactor;
+		return mer + ratedelta;
+	}
+
 
 }
